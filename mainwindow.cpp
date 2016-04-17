6354ea93fe287c9df5e273ac4f21f252232c49c1
PeihaoYuan <huowenanfashi@vip.qq.com>
2016-04-17 22:38:26 +0100

    Added files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,61 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QThread>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::timerEvent(QTimerEvent *)
+{
+    QFile file("Frequency1");
+    if(!file.open(QIODevice::ReadOnly))
+            QMessageBox::information(0,"info",file.errorString());
+
+    QTextStream in(&file);
+
+    ui->textBrowser->setText(in.readAll());
+
+    QFile file1("Frequency2");
+    if(!file1.open(QIODevice::ReadOnly))
+            QMessageBox::information(0,"info",file1.errorString());
+
+    QTextStream in1(&file1);
+
+    ui->textBrowser_2->setText(in1.readAll());
+
+    QFile file2("Scale1");
+    if(!file2.open(QIODevice::ReadOnly))
+            QMessageBox::information(0,"info",file2.errorString());
+
+    QTextStream in2(&file2);
+
+    ui->textBrowser_7->setText(in2.readAll());
+
+    QFile file3("Scale2");
+    if(!file3.open(QIODevice::ReadOnly))
+            QMessageBox::information(0,"info",file3.errorString());
+
+    QTextStream in3(&file3);
+
+    ui->textBrowser_8->setText(in3.readAll());
+
+    QFile file4("Scale3");
+    if(!file4.open(QIODevice::ReadOnly))
+            QMessageBox::information(0,"info",file4.errorString());
+
+    QTextStream in4(&file4);
+
+    ui->textBrowser_9->setText(in4.readAll());
+}
---
+++ ui_mainwindow.h
@@ -0,0 +1,144 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 4.8.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QStatusBar>
+#include <QtGui/QTextBrowser>
+#include <QtGui/QToolBar>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QTextBrowser *textBrowser;
+    QTextBrowser *textBrowser_2;
+    QTextBrowser *textBrowser_3;
+    QTextBrowser *textBrowser_4;
+    QTextBrowser *textBrowser_5;
+    QTextBrowser *textBrowser_6;
+    QTextBrowser *textBrowser_7;
+    QTextBrowser *textBrowser_8;
+    QTextBrowser *textBrowser_9;
+    QMenuBar *menuBar;
+    QMenu *menuScale_Tuner;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(324, 304);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        textBrowser = new QTextBrowser(centralWidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setGeometry(QRect(50, 60, 81, 51));
+        textBrowser_2 = new QTextBrowser(centralWidget);
+        textBrowser_2->setObjectName(QString::fromUtf8("textBrowser_2"));
+        textBrowser_2->setGeometry(QRect(150, 60, 81, 51));
+        textBrowser_3 = new QTextBrowser(centralWidget);
+        textBrowser_3->setObjectName(QString::fromUtf8("textBrowser_3"));
+        textBrowser_3->setGeometry(QRect(230, 60, 61, 51));
+        textBrowser_4 = new QTextBrowser(centralWidget);
+        textBrowser_4->setObjectName(QString::fromUtf8("textBrowser_4"));
+        textBrowser_4->setGeometry(QRect(10, 0, 301, 51));
+        textBrowser_5 = new QTextBrowser(centralWidget);
+        textBrowser_5->setObjectName(QString::fromUtf8("textBrowser_5"));
+        textBrowser_5->setGeometry(QRect(20, 120, 281, 51));
+        textBrowser_6 = new QTextBrowser(centralWidget);
+        textBrowser_6->setObjectName(QString::fromUtf8("textBrowser_6"));
+        textBrowser_6->setGeometry(QRect(130, 60, 21, 51));
+        textBrowser_7 = new QTextBrowser(centralWidget);
+        textBrowser_7->setObjectName(QString::fromUtf8("textBrowser_7"));
+        textBrowser_7->setGeometry(QRect(140, 180, 51, 51));
+        textBrowser_8 = new QTextBrowser(centralWidget);
+        textBrowser_8->setObjectName(QString::fromUtf8("textBrowser_8"));
+        textBrowser_8->setGeometry(QRect(50, 180, 81, 51));
+        textBrowser_9 = new QTextBrowser(centralWidget);
+        textBrowser_9->setObjectName(QString::fromUtf8("textBrowser_9"));
+        textBrowser_9->setGeometry(QRect(200, 180, 81, 51));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 324, 27));
+        menuScale_Tuner = new QMenu(menuBar);
+        menuScale_Tuner->setObjectName(QString::fromUtf8("menuScale_Tuner"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuScale_Tuner->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
+        textBrowser->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Roboto'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", 0, QApplication::UnicodeUTF8));
+        textBrowser_2->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Roboto'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", 0, QApplication::UnicodeUTF8));
+        textBrowser_3->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Roboto'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:20pt;\">Hz</span></p></body></html>", 0, QApplication::UnicodeUTF8));
+        textBrowser_4->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Roboto'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:20pt;\">Freq (Actual/Standard)</span></p></body></html>", 0, QApplication::UnicodeUTF8));
+        textBrowser_5->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Roboto'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:20pt;\">Scale (5% Deviation)</span></p></body></html>", 0, QApplication::UnicodeUTF8));
+        textBrowser_6->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Roboto'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:20pt;\">/</span></p></body></html>", 0, QApplication::UnicodeUTF8));
+        menuScale_Tuner->setTitle(QApplication::translate("MainWindow", "Scale Tuner", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
